c9279c8ea3e9a9a5c35ecc731029965715496fe0
Sean Holstrom <basementbebop@gmail.com>
2015-01-09 10:26:08 -0800

    Add second spec

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,7 +2,13 @@ require('rspec')
 require('word_count')
 
 describe('String#word_count') do
-  it("the method word_count recognizes the argument is the same as the object and returns the amount of times it appears") do
+
+  it("recognizes the argument is the same as the object and returns the amount of times it appears") do
     expect(("mantis").word_count("mantis")).to(eq(1))
   end
+
+  it("recognizes the argument is listed more than once in the object and returns the amount of times it appears") do
+    expect(("mantis mantis").word_count("mantis")).to(eq(2))
+  end
+
 end
