b2aceb75a214b58f6e1a2e1242615c7d80ef904a
Sean Holstrom <basementbebop@gmail.com>
2015-01-09 12:35:57 -0800

    Update plural result

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ get('/display') do
   @result = params.fetch('text_block').word_count(params.fetch('word'))
   @word = params.fetch('word')
   @plural = "s"
-  if @result < 2
+  if @result < 2 and @result > 0
     @plural = ""
   end
   erb(:display)
