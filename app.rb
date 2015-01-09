184ad61aa2ace1000463df8ed719d2483d9cbbee
Sean Holstrom <basementbebop@gmail.com>
2015-01-09 12:54:07 -0800

    Add error messages

--- app.rb
+++ app.rb
@@ -10,9 +10,17 @@ end
 get('/display') do
   @result = params.fetch('text_block').word_count(params.fetch('word'))
   @word = params.fetch('word')
+  @text_block = params.fetch('text_block')
   @plural = "s"
   if @result < 2 and @result > 0
     @plural = ""
+  end
+  @error = ""
+  if @word = ""
+    @error = "Maybe it appears 0 times because you didn't put in a word..."
+  end
+  if @text_block = ""
+    @error = "Next time actually putting in text might be nice..."
   end
   erb(:display)
 end
